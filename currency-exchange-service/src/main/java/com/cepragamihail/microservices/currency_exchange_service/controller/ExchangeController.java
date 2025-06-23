de18d5d64e8d5314cad851d1e2425e58b164af96
Mihail Cepraga <yahim09@gmail.com>
2025-06-23 11:41:37 +0300

    init config and controller

---
+++ currency-exchange-service/src/main/java/com/cepragamihail/microservices/currency_exchange_service/controller/ExchangeController.java
@@ -0,0 +1,19 @@
+package com.cepragamihail.microservices.currency_exchange_service.controller;
+
+import com.cepragamihail.microservices.currency_exchange_service.models.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/currency-exchange")
+public class ExchangeController {
+
+    @GetMapping("/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/cepragamihail/microservices/currency_exchange_service/models/CurrencyExchange.java
@@ -0,0 +1,51 @@
+package com.cepragamihail.microservices.currency_exchange_service.models;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal rate;
+
+    public CurrencyExchange() {}
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal rate) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
