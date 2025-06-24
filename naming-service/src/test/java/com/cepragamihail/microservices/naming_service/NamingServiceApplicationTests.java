b915a799593934249a3b0160cc8b5d0da2624f3d
Mihail Cepraga <yahim09@gmail.com>
2025-06-24 13:36:07 +0300

    init Eureka Naming Server

---
+++ naming-service/src/test/java/com/cepragamihail/microservices/naming_service/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepragamihail.microservices.naming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
